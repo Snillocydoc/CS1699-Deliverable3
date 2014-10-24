64e7d8f8992dcd587ad2328c0e4ae99c0c410287
Cody Collins <snillocydoc@yahoo.com>
2014-10-24 18:03:12 -0400

    Created stub of Game Menu step definitions

--- src/Test/JavaSteps/GameMenuStepDefs.java
+++ src/Test/JavaSteps/GameMenuStepDefs.java
@@ -1,5 +1,46 @@
 package Test.JavaSteps;
-import 
-public class GameMenuStepDefs {
 
+
+import cucumber.api.java.en.*;
+
+public class GameMenuStepDefs
+{
+	
+	@Given("^a Tetris game is running")
+	public void gameIsRunning()
+	{
+		
+	}
+	@Given("^a Tetris game is paused")
+	public void gameIsPaused()
+	{
+		
+	}
+	@Given("^a Tetris game is not running or paused")
+	public void gameIsNotRunningOrPaused()
+	{
+		
+	}
+	
+	@When("^the user selects the Game menu")
+	public void selectsGameMenu()
+	{
+		
+	}
+	
+	@Then("^the running Tetris game should pause")
+	public void gamePauses()
+	{
+		
+	}
+	@Then("^the paused Tetris game should continue")
+	public void gameContinues()
+	{
+		
+	}
+	@Then("^nothing should occur")
+	public void nothingOccurs()
+	{
+		
+	}
 }
