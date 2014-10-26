ba89c9e7b8e6747c5c667740a068d7205253ac71
Cody Collins <snillocydoc@yahoo.com>
2014-10-25 21:36:05 -0400

    Get rid of the Exit testing since it calls System.exit()

---
+++ Test/StepDefs.java
@@ -0,0 +1,90 @@
+
+
+
+
+import java.awt.Toolkit;
+import javax.swing.*;
+
+import cucumber.api.java.en.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+
+
+public class StepDefs
+{
+
+	Tetris tetris = new Tetris();
+	TetrisMenuBar menu = new TetrisMenuBar(tetris);
+	TetrisKeyAdapter keyAdapter = new TetrisKeyAdapter(tetris);
+	TetrisWindowAdapter windowAdapter = new TetrisWindowAdapter(tetris);
+	JFrame frame = new JFrame("Tetris");
+	
+	
+	@Given("^a Tetris game is running")
+	public void gameIsRunning()
+	{
+		setup();
+		tetris.start();
+	}
+	@Given("^a Tetris game is paused")
+	public void gameIsPaused()
+	{
+		setup();
+		tetris.start();
+		tetris.pause();
+	}
+	
+	@When("^the user selects the Game menu")
+	public void selectsGameMenu()
+	{
+		menu.getMenu(0).doClick();
+		
+	}
+	@When("the user selects the \"+(.*)\" button")
+	public void selectButton(String name)
+	{
+		System.out.println(name);
+		int count = menu.getMenu(0).getMenuComponentCount();
+	
+		for(int x=0;x<count;x++)
+		{
+			System.out.println(menu.getMenu(0).getMenuComponent(x));
+			if(menu.getMenu(0).getMenuComponent(x).getAccessibleContext().getAccessibleName()!=null&&menu.getMenu(0).getMenuComponent(x).getAccessibleContext().getAccessibleName().equals(name))
+			{
+				System.out.println("ITS NULL");
+				
+				
+			}
+		}
+	}
+	
+	
+	@Then("^the running Tetris game should pause")
+	public void gamePauses()
+	{
+		assertTrue(tetris.isPaused());
+	}
+	@Then("^the paused Tetris game should continue")
+	public void gameContinues()
+	{
+		assertFalse(tetris.isPaused());
+	}
+	
+	
+	public void setup()
+	{
+		frame.add(tetris);
+		//Set the properties of the frame
+		frame.addWindowListener(windowAdapter);
+		frame.setJMenuBar(menu);
+		frame.addKeyListener(keyAdapter);
+		frame.setSize(485,728);
+		frame.setResizable(false);
+		//Centers the frame on the screen
+		frame.setLocation(Toolkit.getDefaultToolkit().getScreenSize().width/2-tetris.getWidth()/2,2);
+		//Shows the frame
+		frame.setVisible(true);
+	}
+	
+}
